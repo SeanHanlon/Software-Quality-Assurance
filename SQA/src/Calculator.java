be117710189a702dba2dc9fd57fae6cac12ffab5
Sean Hanlon <32528632+SeanHanlon@users.noreply.github.com>
2022-09-12 11:34:57 +0200

    Update Calculator.java
    Added a space between variables and operators.

--- SQA/src/Calculator.java
+++ SQA/src/Calculator.java
@@ -4,25 +4,25 @@ public class Calculator {
 	
 	
 	public int add(int number, int numb) {
-		int answer = numb+number;
+		int answer = numb + number;
 		return answer;
 		
 	}
 	
 	public int subtract(int number, int numb) {
-		int answer = number-numb;
+		int answer = number - numb;
 		return answer;
 		
 	}
 	
 	public int multiply(int number, int numb) {
-		int answer = number*numb;
+		int answer = number * numb;
 		return answer;
 		
 	}
 	
 	public int divide(int number, int numb) {
-		int answer = number/numb;
+		int answer = number / numb;
 		return answer;
 		
 	}
